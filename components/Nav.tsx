7deae15d0cd26adf84d0a1c93f252ae8c87c6c66
mahmoudelfeelig <mahmoudelfeelig@gmail.com>
2025-10-30 21:14:13 +0100

    added mobile responsiveness to the navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -112,11 +112,15 @@ export default async function Nav() {
       <details className="nav-mobile">
         <summary>
           <span>Menu</span>
-          <span className="nav-mobile-glyph" aria-hidden="true">
-            ?
-          </span>
+          <span className="nav-mobile-glyph" aria-hidden="true">{'\u2630'}</span>
         </summary>
         <div className="nav-mobile-body">
+          <Link href="/" className="nav-mobile-brand" title="Home">
+            <svg viewBox="0 0 24 24" aria-hidden="true">
+              <path fill="#b0142f" d="M5 21l5-3 1-5-5-5 2-2 5 4 3-1 3-5 2 1-1 6-3 4 1 5-4 1-4 2z" />
+            </svg>
+            <span>Anubis</span>
+          </Link>
           {baseLinks.map((item) => (
             <Link key={item.href} href={item.href}>
               {item.label}
