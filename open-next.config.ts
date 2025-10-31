88229d73dc2a0d8f699086ed3776e351a5c509f8
mahmoudelfeelig <mahmoudelfeelig@gmail.com>
2025-10-31 23:14:28 +0100

    updated the configs for open next

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,2 +1,7 @@
-import { defineConfig } from '@opennextjs/cloudflare';
-export default defineConfig({});
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+
+export default defineCloudflareConfig({
+	incrementalCache: r2IncrementalCache,
+});
